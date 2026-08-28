8579cebed69822fe3f1cefd58ca5250452cea159
gunk230_cloudcli <2512690268@qq.com>
2026-08-28 13:34:40 +0800

    Fix DAPO reward kwargs and rollout repair logging

--- recipe/dapo/main_dapo.py
+++ recipe/dapo/main_dapo.py
@@ -155,6 +155,7 @@ class TaskRunner:
             0,
             max_resp_len=config.data.max_response_length,
             overlong_buffer_cfg=config.reward_model.overlong_buffer,
+            **config.reward_model.get("reward_kwargs", {}),
         )
 
         # Note that we always use function-based RM for validation
@@ -164,6 +165,7 @@ class TaskRunner:
             1,
             max_resp_len=config.data.max_response_length,
             overlong_buffer_cfg=config.reward_model.overlong_buffer,
+            **config.reward_model.get("reward_kwargs", {}),
         )
         resource_pool_manager = ResourcePoolManager(resource_pool_spec=resource_pool_spec, mapping=mapping)
 
